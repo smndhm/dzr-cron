eed9fef8f2cfc9b3a9c360c68a6f36f3bf4c28dd
smndhm <simon.duhem@gmail.com>
2022-02-20 22:55:41 +0100

    fix: remove mention of favorite

--- index.spec.js
+++ index.spec.js
@@ -1,8 +1,8 @@
 jest.mock('./config/crons.conf');
 
-const mockLastFavTracks = jest.fn();
+const mocklastTracks = jest.fn();
 jest.mock('./cron-scripts/last-tracks', () => ({
-	lastFavTracks: mockLastFavTracks,
+	lastTracks: mocklastTracks,
 }));
 
 const mockSyncPlaylists = jest.fn();
@@ -12,7 +12,7 @@ jest.mock('./cron-scripts/sync-playlists', () => ({
 
 describe('Cron index', () => {
 	afterEach(() => {
-		mockLastFavTracks.mockClear();
+		mocklastTracks.mockClear();
 		mockSyncPlaylists.mockClear();
 	});
 
@@ -21,7 +21,7 @@ describe('Cron index', () => {
 		const index = require('./index');
 		expect(index).toBeDefined();
 		jest.advanceTimersByTime(24 * 60 * 60 * 1000);
-		expect(mockLastFavTracks).toBeCalledTimes(2);
+		expect(mocklastTracks).toBeCalledTimes(2);
 		expect(mockSyncPlaylists).toBeCalledTimes(25);
 	});
 });
